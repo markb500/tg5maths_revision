059d730e8f48667c18c7efa602d31f3078944234
markb500 <81427572+markb500@users.noreply.github.com>
2024-12-11 16:01:52 +0000

    Add files via upload

--- stats_module.js
+++ stats_module.js
@@ -17,21 +17,30 @@ function stats(ctx) {
                 e = rndgen(2, 36, 0, 1, -1);
             }while (a === b || a === c || a === d || a === e || b === c || b === d || b === e || c === d || c === e || d === e || 
                         dp(360 / (a + b + c + d + e), 0, -1) !== (360 / (a + b + c + d + e)));
-            sumq += "a = " + a + " b = " + b + " c = " + c + " d = " + d + " e = " + e + " total = " + (a + b + c + d + e) + 
-                                                                                " per deg = " + (360 / (a + b + c + d + e)) + "<br>";
 
-            var startx = 200;
-            var starty =200;
+            var startx = 250;
+            var starty =250;
             var x, y;
-            radius = 120;
+            radius = 100;
             var angleScale = 360 / (a + b + c + d + e);
-            var angles = [["a", a, a / 2 * angleScale * Math.PI / 180], 
-                        ["b", b, (a * angleScale + (b / 2) * angleScale) * Math.PI / 180], 
-                        ["c", c, ((a + b) * angleScale + (c / 2) * angleScale) * Math.PI / 180], 
-                        ["d", d, ((a + b + c) * angleScale + (d / 2) * angleScale) * Math.PI / 180], 
-                        ["e", e, ((a + b + c + d) * angleScale + (e / 2) * angleScale) * Math.PI / 180]];    //Angles for label positions
+            var angles = [["Crisps", a * angleScale, a / 2 * angleScale * Math.PI / 180], 
+                        ["Peanuts", b * angleScale, (a * angleScale + (b / 2) * angleScale) * Math.PI / 180], 
+                        ["Choc bar", c * angleScale, ((a + b) * angleScale + (c / 2) * angleScale) * Math.PI / 180], 
+                        ["Apple", d * angleScale, ((a + b + c) * angleScale + (d / 2) * angleScale) * Math.PI / 180], 
+                        ["Banana", e * angleScale, ((a + b + c + d) * angleScale + (e / 2) * angleScale) * Math.PI / 180]];
+                            //Labels, sector angles in degrees, angle from zero to each sector centre in radians
+            sumq += "The favourite snacks of " + (a + b + c + d + e) + " students are shown in the pie chart below. From the chart<br>";
+            sumq += "a. How many students prefer peanuts.<br>b. How many students prefer a chocolate bar.";
+            suma += "<br>".repeat(9);
+            suma += "$$\\begin{aligned}a. \\frac{360}{" + (a + b + c + d + e) + "}=" + (360/(a+b+c+d+e)) + "\\ degrees\\ per\\ student.\\\\[5pt]";
+            suma += "For\\ peanuts,\\ \\frac{" + (b * angleScale) + "}{" + (360/(a+b+c+d+e)) + "}&=\\underline{\\mathbf{" + ((b * angleScale) / (360/(a+b+c+d+e))) + 
+                        "\\ students\\ prefer\\ peanuts}}\\\\[5pt]";
+            suma += "b.\\frac{360-(" + angles[0][1] + "+" + angles[1][1] + "+" + angles[3][1] + "+" + angles[4][1] + ")}{" + (360/(a+b+c+d+e)) + 
+                        "}&=\\underline{\\mathbf{" + (360 - (angles[0][1] + angles[1][1] + angles[3][1] + angles[4][1])) / (360/(a+b+c+d+e)) + 
+                        "\\ students\\ prefer\\ a\\ chocolate\\ bar.}}\\\\[5pt]";
+            suma += "\\end{aligned}$$";
             ctx.strokeStyle = '#0000ff';
-            ctx.arc(200, 200, radius, 0, 2 * Math.PI);
+            ctx.arc(startx, starty, radius, 0, 2 * Math.PI);
             ctx.moveTo(startx, starty);
             ctx.lineTo(startx + radius * Math.cos(0 * (Math.PI / 180)), 
                             starty + radius * Math.sin(0 * (Math.PI / 180)));
@@ -48,7 +57,6 @@ function stats(ctx) {
             ctx.lineTo(startx + radius * Math.cos(((a + b + c + d) * angleScale) * (Math.PI / 180)), 
                             starty + radius * Math.sin(((a + b + c + d ) * angleScale) * (Math.PI / 180)));
             ctx.stroke();
-            // ctx.moveTo(startx + (radius + 10) * Math.sin(a / 2 * (Math.PI / 180)), starty + (radius + 10) * Math.cos(a / 2 * (Math.PI / 180)))
             ctx.font = "20px Comic Sans MS";
             ctx.textAlign = "left";
             for (let i = 0; i < 5; i++) {
@@ -59,19 +67,13 @@ function stats(ctx) {
                 } else {
                     ctx.textAlign = "right";
                 }
-                ctx.fillText(angles[i][0] + " = " + angles[i][1], x, y);
+                if (i === 2) {
+                    ctx.fillText(angles[i][0], x, y);
+                } else {
+                    ctx.fillText(angles[i][0] + " = " + angles[i][1] + "\xB0", x, y);
+                }
                 ctx.stroke();
             }
-
-
-
-            // ctx.fillText("a", startx + (radius + 10) * Math.cos(a / 2 * angleScale * (Math.PI / 180)), starty + (radius + 10) * Math.sin(a / 2 * angleScale * (Math.PI / 180)));
-            // ctx.fillText("a = " + a, x, y);
-            // ctx.fillText("d = " + d, startx + (radius + 10) * Math.cos(45 * Math.PI / 180), starty + 10 - (radius + 10) * Math.sin(45 * Math.PI / 180));
-            // ctx.textAlign = "right";
-            // ctx.fillText("b = 45", startx - (radius + 10) * Math.cos(45 * Math.PI / 180), starty + (radius + 10) * Math.sin(45 * Math.PI / 180));
-            // ctx.fillText("c = 45", startx - (radius + 10) * Math.cos(45 * Math.PI / 180), starty + 10 - (radius + 10) * Math.sin(45 * Math.PI / 180));
-            // ctx.stroke();
             break;
     }
     var notesLink = "images/20230706-MathsBook08Proportionv1_6-APO.pdf#page=4";
